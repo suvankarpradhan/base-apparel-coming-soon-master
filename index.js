5654b7a39cf669a7ca497995a26b659adb90e4cb
Suvankar <suvankarpradhan100@gmail.com>
2021-04-23 19:37:24 +0530

    modify js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 $('.ebtn').click(()=>{
     let email = $('.email-form input').val();
-    console.log(email);
     let mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     if(mailformat.test(email)){
         $('.email-form').css('border-color','hsl(0, 6%, 20%)');
-        $('.error-msg').text('');         
+        $('.error-msg').text('');
+        $('.logo').removeClass('error');         
     }else{
         $('.email-form').css('border-color','hsl(0, 93%, 68%)');
         $('.error-msg').text('Please provide a valid email');
+        $('.logo').addClass('error');       
     }
 })
